277541a8d4be4d9198d4cb2cb839518fb8b6a85a
Sarah Abdo <abdos1@gauss.acsl.newpaltz.edu>
2018-02-19 20:48:47 -0500

    Initial commit

---
+++ hw1/c.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main()
+{
+using namespace std;
+
+int a,b,c, d, e, f, g;
+cout << "Enter first number:" << endl;
+cin >> a;
+
+cout << "Enter first number:" << endl;
+cin >> b;
+
+c = a + b;
+cout << c << endl;
+
+d = a - b;
+cout << d << endl;
+
+e = a * b;
+cout << e << endl;
+
+f = a / b;
+cout << f << endl;
+
+g = a % b;
+cout << g << endl;
+
+return 0;
+}
