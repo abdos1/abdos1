649b6712b5695ceb8a1671799b995dace6675ed3
abdos1 <36493038+abdos1@users.noreply.github.com>
2018-02-26 18:24:58 -0500

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+using namespace std;
+unsigned long bn;
+unsigned long bn1;
+unsigned long c;
+
+cout << "Please enter your number:"<< endl;
+cin >> bn;
+cout << "Please enter a second number:"<<endl;
+cin >> bn1;
+
+cout << "bn + bn1 is:" << bn + bn1 << endl;
+
+
+return 0;
+}
