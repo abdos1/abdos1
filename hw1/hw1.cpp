c0110f5ed9241992f44fd375b4f6ef8320b51f52
Sarah Abdo <abdos1@gauss.acsl.newpaltz.edu>
2018-03-27 19:29:12 -0400

    Initial commit

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -12,16 +12,18 @@ cout << "Enter second number:" << endl;
 cin >> b;
 
 c = a + b;
-cout << c << endl;
+cout << a << "+" << b << "=" << c << endl;
 
 d = a - b;
-cout << d << endl;
+cout << a << "-" << b << "=" <<d << endl;
 
 e = a * b;
-cout << e << endl;
+cout << a << "x" << b << "=" <<e << endl;
 
+//cout << "problem line" << endl;
+//this goes before the line you are unsure of to test if there is a problem
 if ( b == 0 )
-{
+{// scope, similar to int main (smaller)
 cout << "Error" << endl;
 }
 else
@@ -29,12 +31,13 @@ else
 f = a / b;
 };
 
-cout << f << endl;
+cout << a << "/" << b << "="<< f << endl;
 
 
 g = a % b;
-cout << g << endl;
+cout << "remainer=" << g << endl;
 
 
 return 0;
 }
+// floating point exception appears when b = 0 just because it really is an error when i divide by zero
