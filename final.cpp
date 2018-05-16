ae48345ac7655ba2b28c79d7de110d57c48caa47
abdos1 <36493038+abdos1@users.noreply.github.com>
2018-05-16 17:45:26 -0400

    Add files via upload

---
+++ final.cpp
@@ -0,0 +1,18 @@
+#include "tinyxml2.h"
+#include <iostream>
+
+using namespace std;
+using namespace tinyxml2;
+int main(int argc, const char** argv)
+
+{
+	XMLDocument*doc= new XMLDocument();
+	doc-> LoadFile(argv[1]);
+	XMLElement*root=doc->FirstChildElement();
+	cout<<root->FirstChildElement()->Name()<<"\n";
+	cout<<root->FirstChildElement()->GetText()<<"\n";
+//I made this
+
+	return 0;
+
+}
